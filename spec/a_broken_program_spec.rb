607418f22f42c5c34a267b1ee12b8d4160a24fe3
Taylor Mills <taylormills24@yahoo.com>
2019-01-26 22:43:14 +0000

    Automatically backed up by Learn

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -3,7 +3,7 @@ describe 'A broken program' do
   it 'to not raise an error when loaded' do
     expect{
       load 'lib/a_working_program.rb'
-    }.to raise_error
+    }.to not raise_error
   end
 
 end
